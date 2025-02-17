3b768446d97a8c94ad9a36f12a0828494e5848f8
caio32pro <caio32823@gmail.com>
2025-02-17 11:48:15 -0300

    First Commit of the exercise

---
+++ android/app/src/main/kotlin/com/example/flutter_application_ex_boasvindas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_ex_boasvindas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/power.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Rangers extends StatelessWidget {
+  const Rangers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //
+    );
+  }
+}
